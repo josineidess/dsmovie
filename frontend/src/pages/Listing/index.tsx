999e6f966d0553eba79e43e0c6fe13f22aa10e34
josineidess <josineide1402@gmail.com>
2022-01-14 21:43:55 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 
 function Listing() {
+  axios.get(`${BASE_URL}/movies`).
+    then(response => {
+      console.log(response.data);
+    })
+  
   return (
     <>
       <Pagination />
